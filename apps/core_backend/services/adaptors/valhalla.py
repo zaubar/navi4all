0d3d27fc61c8c2a3a5a57909a234a63d659c5740
Nihar Thakkar <nihartar@gmail.com>
2026-02-18 09:33:42 +0100

    chore: add file headers

--- apps/core_backend/services/adaptors/valhalla.py
+++ apps/core_backend/services/adaptors/valhalla.py
@@ -1,3 +1,21 @@
+# Navi4All
+# Copyright (C) Navi4All contributors
+# Maintainer: Plan4Better GmbH
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+#
+# Licensed under the GNU Affero General Public License, Version 3 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from core.config import settings
 from httpx import AsyncClient
 from redis import Redis
